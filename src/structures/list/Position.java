052ae1d4ca7911dfc078eec9082677e8702feda6
Edmond Weiss <edmondweiss@gmail.com>
2018-03-18 11:02:52 -0400

    Added Position and PositionalList interfaces

---
+++ src/structures/list/Position.java
@@ -0,0 +1,11 @@
+package structures.list;
+
+public interface Position<E> {
+	/**
+	 * Used to get the element stored at this position.
+	 * 
+	 * @return The stored element in the position.
+	 * @throws IllegalStateException if position has been removed from the list and thus invalid.
+	 */
+	E getElement() throws IllegalStateException;
+}
---
+++ src/structures/list/PositionalList.java
@@ -0,0 +1,41 @@
+package structures.list;
+
+/** An interface for positional lists. */
+
+public interface PositionalList<E> {
+	/** Returns the number of elements in the list. */
+	int size();
+	
+	/** Test whether the list is empty. */
+	boolean isEmpty();
+	
+	/** Returns the first Position in the list (or null, if empty). */
+	Position<E> first();
+	
+	/** Returns the last Position in the list (or null, if empty). */
+	Position<E> last();
+	
+	/** Returns the Position immediately before Position p (or null, if empty). */
+	Position<E> before(Position<E> position); 
+	
+	/** Returns the Position immediately after Position p (or null, if empty). */
+	Position<E> after(Position<E> position);
+	
+	/** Inserts element e at the front of the list and returns its new Position. */
+	Position<E> addFirst(E e);
+	
+	/** Inserts element e at the end of the list and returns its new Position. */
+	Position<E> addLast(E e);
+	
+	/** Inserts element e immediately before Position p returns the replaced element. */
+	E addBefore(Position<E> p, E e) throws IllegalArgumentException;
+	
+	/** Inserts element e immediately after Position p returns the replaced element. */
+	E addAfter(Position<E> p, E e) throws IllegalArgumentException;
+	
+	/**  Replaces the element stored at Position p and returns the replaced element. */
+	E set(Position<E> p, E e) throws IllegalArgumentException;
+
+	/** Removes the element stored at Position p and returns it (invalidating p). */
+	E remove(Position<E> p) throws IllegalArgumentException;
+}
