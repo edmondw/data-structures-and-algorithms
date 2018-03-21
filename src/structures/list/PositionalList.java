178d1597279e64db806b9509be5ebd1524881a8e
Edmond Weiss <edmondweiss@gmail.com>
2018-03-21 13:55:58 -0400

    Added support for remove methods

--- src/structures/list/PositionalList.java
+++ src/structures/list/PositionalList.java
@@ -27,11 +27,23 @@ public interface PositionalList<E> {
 	/** Inserts element e at the end of the list and returns its new Position. */
 	Position<E> addLast(E e);
 	
+	/** Removes the element at the front of the list and returns the element. */
+	E removeFirst();
+	
+	/** Removes the element at the tail of the list and returns the element. */
+	E removeLast();
+	
 	/** Inserts element e immediately before Position p returns the new Position. */
 	Position<E> addBefore(Position<E> p, E e) throws IllegalArgumentException;
 	
 	/** Inserts element e immediately after Position p returns the new Position. */
 	Position<E> addAfter(Position<E> p, E e) throws IllegalArgumentException;
+	
+	/** Removes element e immediately before Position p and returns the element. */
+	E removeBefore(Position<E> p);
+	
+	/** Removes element e immediately after Position p and returns the element. */
+	E removeAfter(Position<E> p);
 	
 	/**  Replaces the element stored at Position p and returns the replaced element. */
 	E set(Position<E> p, E e) throws IllegalArgumentException;
