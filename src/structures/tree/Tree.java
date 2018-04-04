64a4a6ebe6c58f110857b26ee4808b3f8993c1a7
Edmond Weiss <edmondweiss@gmail.com>
2018-04-04 16:53:44 -0400

    Create unfinished tree interface

---
+++ src/structures/tree/Tree.java
@@ -0,0 +1,40 @@
+package structures.tree;
+
+import structures.list.Position;
+
+public interface Tree<E> extends Iterable<E> {
+  /** Returns the root of the tree. */
+  Position<E> root();
+  
+  /** Returns the parent position of p. */
+  Position<E> parent(Position<E> p) throws IllegalArgumentException;
+  
+  /** Returns an iterable collection of positions of position p. */
+  Iterable<Position<E>> children(Position<E>  p) throws IllegalArgumentException;
+  
+  /** Returns the number of children of position of p. */
+  int numChildren(Position<E> p) throws IllegalArgumentException;
+  
+  /** Returns true if position p is an external(leaf: having no children) node. */
+  boolean isExternal(Position<E>  p) throws IllegalArgumentException;
+  
+  /** Returns true if position p is an internal node (having 1 more children). */
+  boolean isInternal(Position<E> p) throws IllegalArgumentException;
+  
+  /** Returns true if position p is the root of the tree. */
+  boolean isRoot(Position<E> p) throws IllegalArgumentException;
+  
+  /** Returns the number of positions (and thus number of elements) in the tree. */
+  int size();
+  
+  /** Returns true if the tree contains no positions (thus no elements). */
+  boolean isEmpty();
+  
+  /** Returns an iterator for all elements in the tree (so that the tree itself is Iterable). */
+  Iterator<E> iterator();
+  
+  /** Returns an iterable collection of all positions of the tree. */
+  Iterable<Position<E>> position(); 
+  
+  
+}
